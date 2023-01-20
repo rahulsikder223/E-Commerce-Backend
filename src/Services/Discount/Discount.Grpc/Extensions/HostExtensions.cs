1b344807d776a8d4f3a419f0782c5fc81ddd4a05
Bishwadeep Sikder <rahulsikder223@gmail.com>
2023-01-21 04:51:51 +0530

    Discount.Grpc complete

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating PostgreSQL Database...");
+
+                    using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand { Connection = connection };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    // Seed Data...
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('iPhone X', 'iPhone X Discount', 150)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('Samsung S22', 'Samsung S22 Discount', 120)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("PostgreSQL Database Migration Complete...");
+                }
+                catch (Exception)
+                {
+                    logger.LogError("Error while migrating PostgreSQL Database...");
+
+                    if (retryAvailability < 10)
+                    {
+                        retryAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryAvailability);
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
